4978e7e7ca363a1d357d044e36ed9e688c2047db
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-09-05 01:00:16 -0700

    Add association between User (event creator) and Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_05_070807) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_075813) do
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
