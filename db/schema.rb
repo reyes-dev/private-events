2ff1474f576697808605c8ca8be42be4b14e7cbf
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-09-05 20:37:11 -0700

    Allow user to signup with username

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_090019) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -29,7 +28,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_05_090019) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
