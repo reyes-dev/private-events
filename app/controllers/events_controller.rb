593d28b5e3f82000585a9df2221bf98c8973727c
Jarrod Reyes <jarrodreyes98@gmail.com>
2022-09-09 15:35:55 -0700

    Separate past and future events with class models

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,9 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @events = Event.all
+    # @events = Event.all
+    @past_events = Event.past
+    @future_events = Event.future
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,12 @@ class Event < ApplicationRecord
   has_many :event_users, foreign_key: :attended_event_id
   has_many :attendees, through: :event_users, source: :event_attendee
   belongs_to :creator, class_name: "User"
+
+  def self.past
+    self.where("date < ?", Time.now)
+  end
+
+  def self.future
+    self.where("date > ?", Time.now)
+  end
 end
